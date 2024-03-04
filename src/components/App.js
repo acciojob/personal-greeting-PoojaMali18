24384c37ed1034811bab27bef34c707115f1ecb6
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2024-03-04 15:39:11 +0000

    s

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
 import React from "react";
+import {useState} from "react"
 import './../styles/App.css';
 
 const App = () => {
+
+   const [name,setName] = useState("");
+  
+  
+    
+
   return (
     <div>
         {/* Do not remove the main div */}
+       <form>
+        <input onChange={(e)=>setName(e.target.value)} placeholder="enter name"></input>
+        </form>     
+      <h1>Hello{name}</h1>
     </div>
   )
 }
